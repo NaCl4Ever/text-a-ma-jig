a15de19469252b9f0152706fd75962bb48ad4689
Jordan Pectyo <mrpectyo@gmail.com>
2022-03-17 13:38:19 -0400

    Adding jest for unit tests of freq parser

---
+++ src/util/frequency.test.ts
@@ -0,0 +1,19 @@
+import { frequencyParser } from "./frequency";
+
+
+describe('Frequency Parser', () => {
+  test('It should return a proper frequency array', () => {
+    const text = 'test Test test tEsT text text parse parse persa..  possibly text parsa';
+    const results = frequencyParser(text, 2);
+    expect(results.length).toBe(2);
+    expect(results[0][0]).toBe('test');
+    expect(results[0][1]).toBe(4)
+    expect(results[1][0]).toBe('text');
+    expect(results[1][1]).toBe(3);
+  })
+  test('It should throw errors with bad inputs', () => {
+     expect(() => {frequencyParser(null, null)}).toThrowError();
+     expect(() => {frequencyParser('Test amount', null)}).toThrowError();
+     expect(() => {frequencyParser('text test trim', 4)}).toThrowError();
+  })
+})
--- src/util/frequency.ts
+++ src/util/frequency.ts
@@ -1,5 +1,10 @@
 
-export const frequencyParser = (text, amount) => {
+export const frequencyParser = (text: string, amount: number) => {
+  if(!text || !amount) 
+  {
+    throw new Error('Please provide a valid text to parse with a numeric for the amount of frequencies.')
+  }
+
   let freqLookup = new Map();
   //There may be a better to do all this processing to get just the words that speeds up the process
   let terms: string[] = text
@@ -8,11 +13,15 @@ export const frequencyParser = (text, amount) => {
   .filter(t => t.length !== 0)
   .map(t => t.toLowerCase())
 
+  if(amount > new Set(terms).size)
+  {
+    throw new Error('Amount can not exceed the number of unique terms.');
+  }
+  
   for(const term of terms) {
     freqLookup.set(term, freqLookup.has(term) ? freqLookup.get(term) + 1 : 1);
   }
 
   //Preferably get these sorted as we put them in hash map for speed
   return [...freqLookup.entries()].sort((a, b) =>  b[1] - a[1]).slice(0, amount);
-
 }
